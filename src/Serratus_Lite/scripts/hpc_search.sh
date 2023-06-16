91998d89b6ca0ed1aaf9e64924b28a84f9eb10a1
Jacob Paras <jacob.paras@macbook-pro.czbiohub.org>
2023-06-16 11:09:59 -0700

    edited hpc_search

--- src/Serratus_Lite/scripts/hpc_search.sh
+++ src/Serratus_Lite/scripts/hpc_search.sh
@@ -22,51 +22,54 @@ readarray -t accessions < <(cat "${accessions_list}")
 
 declare -x idx=$(( ${SLURM_ARRAY_TASK_ID} -1))
 
-#working dir
-working_dir=${basedir}/Serratus_Lite_pipeline/zpoxv # <---- adjust this when switching query!!!!
-cd $working_dir
+# #working dir
+# working_dir=${basedir}/Serratus_Lite_pipeline/zpoxv # <---- adjust this when switching query!!!!
+# cd $working_dir
 
-#setup query
-query=zpoxv.fasta # <---- adjust this when switching query!!!!
+# #setup query
+# query=zpoxv.fasta # <---- adjust this when switching query!!!!
 
 # run bowtie2
 # -f <input is fasta>  -U <input is unpaired>
+for VIRALSEQ in ${virus_files}
+do
+    query="$(basename "${VIRALSEQ}")"
+    virus_name="$(basename "${VIRALSEQ%.*}")"
+    working_dir="${basedir}/results/${virus_name}"
+    FQ1="${seqbasedir}/${accessions[$idx]}/Unmapped.out.mate1.filteredbyBT.dedup.gsnapFiltered.fastq.gz"
+    FQ2="${seqbasedir}/${accessions[$idx]}/Unmapped.out.mate2.filteredbyBT.dedup.gsnapFiltered.fastq.gz"
 
-FQ1=${seqbasedir}/${accessions[$idx]}/Unmapped.out.mate1.filteredbyBT.dedup.gsnapFiltered.fastq.gz
-FQ2=${seqbasedir}/${accessions[$idx]}/Unmapped.out.mate2.filteredbyBT.dedup.gsnapFiltered.fastq.gz
-VIRALSEQ="${basedir}/query_virus_data/${query}"
-
-if [ -e $FQ1 ] && [ -e $FQ2 ]
-then
-    #PE
-    bowtie2 --quiet --very-sensitive-local \
-    --rg-id na --rg LB:na --rg SM:na \
-    --rg PL:na --rg PU:na \
-    -x ${VIRALSEQ} -1 $FQ1 -2 $FQ2 -q | \
-    samtools view -b -F 4 - > $working_dir/bam/${accessions[$idx]}.bam
-else
-    if [ -e $FQ1 ]
+    if [ -e $FQ1 ] && [ -e $FQ2 ]
     then
-        #SE
+        #PE
         bowtie2 --quiet --very-sensitive-local \
         --rg-id na --rg LB:na --rg SM:na \
         --rg PL:na --rg PU:na \
-        -x ${VIRALSEQ} -U $FQ1 -q | \
+        -x ${VIRALSEQ} -1 $FQ1 -2 $FQ2 -q | \
         samtools view -b -F 4 - > $working_dir/bam/${accessions[$idx]}.bam
+    else
+        if [ -e $FQ1 ]
+        then
+            #SE
+            bowtie2 --quiet --very-sensitive-local \
+            --rg-id na --rg LB:na --rg SM:na \
+            --rg PL:na --rg PU:na \
+            -x ${VIRALSEQ} -U $FQ1 -q | \
+            samtools view -b -F 4 - > $working_dir/bam/${accessions[$idx]}.bam
+        fi
     fi
-fi
 
-#Summarizer
-SUMZER_COMMENT=$(echo sra="na",genome="${query}",version=,date=$(date +%y%m%d-%R))
-SUMZER_TSV="${basedir}/query_virus_data/${query}.sumzer.tsv"
-summarizer="python3 serratus_summarizer.py /dev/stdin ${SUMZER_TSV} $working_dir/summarized/${accessions[$idx]}.summary /dev/null"
+    #Summarizer
+    # SUMZER_COMMENT=$(echo sra="na",genome="${query}",version=,date=$(date +%y%m%d-%R))
+    SUMZER_TSV="${basedir}/query_virus_data/${query}.sumzer.tsv"
+    summarizer="python3 serratus_summarizer.py /dev/stdin ${SUMZER_TSV} $working_dir/summarized/${accessions[$idx]}.summary /dev/null"
 
-if [ -e $working_dir/bam/${accessions[$idx]}.bam ]
-then
-    samtools view $working_dir/bam/${accessions[$idx]}.bam | $summarizer
-    rm $working_dir/bam/${accessions[$idx]}.bam
-fi
-
+    if [ -e $working_dir/bam/${accessions[$idx]}.bam ]
+    then
+        samtools view $working_dir/bam/${accessions[$idx]}.bam | $summarizer
+        rm $working_dir/bam/${accessions[$idx]}.bam
+    fi
+done
 
 
 
