ac0578d85e7da87a8c36cb72fd5ba89d7a0801f6
Jacob Paras <jacob.paras@login-01.clusternet>
2023-06-21 10:22:38 -0700

    added cd in subshell in step 3

--- src/Serratus_Lite/step3.sh
+++ src/Serratus_Lite/step3.sh
@@ -4,6 +4,8 @@
 # WAIT FOR HPC TO FINISH STEP 2
 WORKING_DIR=$(realpath .)
 for VIRUS_DIR in results/* ; do
+    (
     cd "$(realpath "${VIRUS_DIR}")"
     python3 "${WORKING_DIR}/scripts/aggregate_summarized_files.py" --dir .
+    )
 done
