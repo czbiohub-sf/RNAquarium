80eee5fd733b5fbf856532b10bebc6331cf160dd
Jacob Paras <jacob.paras@macbook-pro.czbiohub.org>
2023-06-23 10:03:56 -0700

    first draft of step 1 check py

--- src/nonhost/step.1_check.py
+++ src/nonhost/step.1_check.py
@@ -1,7 +1,27 @@
 from argparse import ArgumentParser
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import itertools
+import json
 import os
+import subprocess as sp
 import sys
+
+
+star_dir = 'STAR_out/'
+pair_dir = 'PE'
+single_dir = 'SE'
+counts_dir = 'counts'
+data_dir = 'data'
+accessions_file = 'SRA_accession_list.1.27.23.txt'
+failed_file = 'SRA_failed_runs.txt'
+results_file = 'results.json'
+log_file = 'Log.final.out'
+mate1_file = 'Unmapped.out.mate1.gz'
+mate2_file = 'Unmapped.out.mate2.gz'
+htseq_count_file = 'htseq-count.txt'
+min_file_size = 10
+num_files = 54_189
+num_cores = 60
 
 
 class MyParser(ArgumentParser):
@@ -21,28 +41,104 @@ def parse_args():
     return vars(config)
 
 
-def check_run(run):
-    pass
-
-
 config_dict = parse_args()
 working_dir = os.path.realpath(config_dict['dir'].rstrip('/').rstrip('\\'))
-
-star_dir = 'STAR_out/'
-accessions_file = 'SRA_accession_list.1.27.23.txt'
-
 star_path = os.path.join(working_dir, star_dir)
-accessions_path = os.path.join(working_dir, accessions_file)
+accessions_path = os.path.join(working_dir, data_dir, accessions_file)
+failed_path = os.path.join(working_dir, data_dir, failed_file)
+results_path = os.path.join(working_dir, data_dir, results_file)
 
-num_files = 54189
-num_cores = 60
 
+def check_run(accession):
+    PE = False
+    SE = False
+    nonexistent = False
+    size_fail = False
+    success = True
+    error_message = ''
+    counts_path = os.path.join(star_path, counts_dir, accession)
+    pair_path = os.path.join(star_path, pair_dir, accession)
+    single_path = os.path.join(star_path, single_dir, accession)
+
+    # creating paths using os.path.join
+    counts_files = itertools.starmap(os.path.join, [(counts_path, log_file), (counts_path, htseq_count_file)])
+    PE_files = itertools.starmap(os.path.join, [(pair_path, log_file), (pair_path, mate1_file), (pair_path, mate2_file)])
+    SE_files = itertools.starmap(os.path.join, [(single_path, log_file), (single_path, mate1_file)])
+
+    # creating a mask over the list of paths to see whether or not the files exist
+    counts_files_existence_mask = map(os.path.isfile, counts_files)
+    PE_files_existence_mask = map(os.path.isfile, PE_files)
+    SE_files_existence_mask = map(os.path.isfile, SE_files)
+
+    # checking counts directory files
+    if not all(counts_files_existence_mask):
+        nonexistent = True
+    # checking PE files
+    if any(PE_files_existence_mask):
+        PE = True
+        if not all(PE_files_existence_mask):
+            nonexistent = True
+    # checking SE files
+    elif not all(SE_files_existence_mask):
+        nonexistent = True
+
+    # handling nonexistent files
+    # flip the values of the masks to get all the files that didn't pass
+    if nonexistent:
+        success = False
+        if PE:
+            nonexistent_files = itertools.compress(data=itertools.chain(counts_files, PE_files),
+                                                   selectors=(not f for f in itertools.chain(counts_files_existence_mask, PE_files_existence_mask)))
+        else:
+            nonexistent_files = itertools.compress(data=itertools.chain(counts_files, SE_files),
+                                                   selectors=(not f for f in itertools.chain(counts_files_existence_mask, SE_files_existence_mask)))
+        msg = f'Nonexistent files: {*nonexistent_files,}'
+        return {'id': accession, 'success': success, 'msg': error_message}
+
+    if PE:
+        PE_size_check = itertools.starmap(os.path.join, [(counts_path, htseq_count_file), (pair_path, mate1_file), (pair_path, mate2_file)])
+        # mask to check if file size is larger than an arbitrarily small, but nonzero, size
+        PE_files_size_mask = map(lambda f: os.path.getsize(f) < min_file_size, PE_size_check)
+        # empty compression means files passed and will evaluate to False
+        if failed_files := list(itertools.compress(data=PE_size_check, selectors=PE_files_size_mask)):
+            size_fail = True
+            error_message += f'Files failed size check: {*failed_files,}\n'
+
+        zipped_files = itertools.starmap(os.path.join, [(pair_path, mate1_file), (pair_path, mate2_file)])
+        # checking if .gzip files are properly zipped
+        PE_zip_mask = map(lambda f: sp.getstatusoutput(f'gzip -t {f}'), zipped_files)
+        # empty compression means files passed and will evaluate to False
+        if not_compressed := list(itertools.compress(data=zipped_files, selectors=PE_zip_mask)):
+            size_fail = True
+            error_message += f'Files not compressed properly: {*not_compressed,}'
+    # SE
+    else:
+        SE_size_check = itertools.starmap(os.path.join, [(counts_path, htseq_count_file), (single_path, mate1_file)])
+        SE_files_size_mask = map(lambda file: os.path.getsize(file) > min_file_size, SE_size_check)
+        if failed_files := list(itertools.compress(data=SE_size_check, selectors=SE_files_size_mask)):
+            size_fail = True
+            error_message += f'Files failed size check: {*failed_files,}\n'
+        zipfile = os.path.join(single_path, mate1_file)
+        # 1 on failure which evaluates to True, 0 on success which evaluates to False
+        if sp.getstatusoutput(f'gzip -t {zipfile}'):
+            size_fail = True
+            error_message += f'Files not compressed properly: {zipfile}'
+
+    success = False if size_fail else True
+    error_message = None if success else error_message
+
+    return {'id': accession, 'success': success, 'msg': error_message}
 
 def main():
-    with open(accessions_file, 'r') as f:
+    with open(accessions_path, 'r') as f:
         accessions = [line.strip() for line in f]
-    with ProcessPoolExecutor(max_workers=num_cores) as ex:
-        results = ex.map(check_run, accessions, chunksize=num_files // num_cores)
+    with ProcessPoolExecutor(max_workers=num_cores) as executor:
+        results = executor.map(check_run, accessions, chunksize=num_files // num_cores)
+    with open(failed_path, 'w') as f:
+        for result in results:
+            f.write(f'{result["id"]}\n')
+    with open(results_path, 'w') as f:
+        json.dump(results, f)
 
 
 if __name__ == '__main__':
