5544d68596e1e4f6a364b5fb512710d7ea7df358
Jacob Paras <jacob.paras@macbook-pro.czbiohub.org>
2023-06-27 17:27:18 -0700

    edited wrapper

--- src/nonhost/step.1.start.sh
+++ src/nonhost/step.1.start.sh
@@ -16,4 +16,39 @@ ACCESSIONS_LIST=${accessions_list:-${DEFAULT_ACCESSIONS_LIST}}
 read -rep $"Enter the directory that has your executables (press enter to use default ${DEFAULT_TOOLS}):"$'\n' tools
 TOOLS=${tools:-${DEFAULT_TOOLS}}
 
+SLURM_OUTDIR="slurm.out"
+
+echo "removing folder $SLURM_OUTDIR"
+rm -rf $SLURM_OUTDIR
+mkdir $SLURM_OUTDIR
+
+echo "removing folder fastq"
+rm -rf fastq
+mkdir fastq
+
+echo "removing folder prefetched"
+rm -rf prefetched
+mkdir prefetched
+
+echo "checking logs directory"
+if [ ! -d "${WORKING_DIR}/logs" ]
+    then
+        mkdir "${WORKING_DIR}/logs"
+fi
+
+echo "removing previous log files"
+
+if [ -e $log0 ]
+    then
+        rm $log0
+fi
+if [ -e $log1 ]
+    then
+        rm $log1
+fi
+if [ -e $log2 ]
+    then
+        rm $log2
+fi
+
 sbatch step.1.fastqDump.sh "${WORKING_DIR}" "${ACCESSIONS_LIST}" "${TOOLS}"
