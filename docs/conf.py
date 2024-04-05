05a8a6e4b1792a0191e7a08469282a552af3e408
Yttria Aniseia <yttria.aniseia@czbiohub.org>
2024-04-05 15:35:55 -0700

    some documentation progress

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Zebrafish RNAquarium Nonhost Read Pipeline'
+copyright = '2023, Yttria Aniseia'
+author = 'Yttria Aniseia'
+release = '0.0.1a'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+myst_enable_extensions = ["attrs_block"]
+myst_number_code_blocks = ["bash", "groovy"]
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+.. Zebrafish RNAquarium Nonhost Read Pipeline documentation master file, created by
+   sphinx-quickstart on Sat Oct 21 13:53:07 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+# Welcome to Zebrafish RNAquarium Nonhost Read Pipeline's documentation!
+
+```{toctree}
+:caption: Contents
+:maxdepth: 2
+gettingstarted.md
+parameters.md
+tips.md
+pipeline-explanation.md
+technical-notes.md
+```
+
+
+
+# Indices and tables
+
+* {ref}`genindex`
+* {ref}`modindex`
+* {ref}`search`
