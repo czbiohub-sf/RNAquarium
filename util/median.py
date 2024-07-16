60b39eff032ebe315c02355fd257337807da4123
Yttria Aniseia <yttria.aniseia@czbiohub.org>
2024-07-16 09:38:13 -0700

    remove old median script

--- util/median.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO
-import numpy as np
-import sys
-
-arr = []
-with open(str(sys.argv[1])) as f:
-    for record in SeqIO.parse(f, "fastq"):
-        arr.append(len(record.seq))
-print(int(np.median(arr)))
