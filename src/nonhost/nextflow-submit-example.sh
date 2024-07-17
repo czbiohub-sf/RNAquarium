e7a99ef4d22fa970db09ecc5ab2680911515f697
Yttria Aniseia <yttria.aniseia@czbiohub.org>
2024-07-17 14:13:16 -0700

    update bruno example run scripts with params-file

--- src/nonhost/nextflow-submit-example.sh
+++ src/nonhost/nextflow-submit-example.sh
@@ -9,23 +9,7 @@
 module load nextflow
 module load mamba
 
-ACCESSIONS=data/SRA_accessions_list.test.txt
-
 set -o verbose
 NXF_OPTS="-Xms500M -Xmx16G" PATH=$PATH:$PWD/bin nextflow run main.nf \
-	--accession-list $ACCESSIONS \
-        --ref-genome /hpc/projects/balla_group/sra_experiments/tools/STAR/Danio_rerio.GRCz11.dna_sm.primary_assembly.fa \
-        --ref-genome-gtf /hpc/projects/balla_group/sra_experiments/tools/STAR/Danio_rerio.GRCz11.108.gtf \
-        --ercc-fa /hpc/projects/balla_group/sra_experiments/tools/STAR/ERCC92.fa \
-        --ercc-gtf /hpc/projects/balla_group/sra_experiments/tools/STAR/ERCC92.gtf \
-	--genome-size 1396431182 --skip-host-counts false \
-	--hisat-use-transcript true \
-	--star-threads-small 4 --star-threads-large 16 --max-cpus 128 --max-memory "256GB" \
-	--star-use-shared-mem false --parallel-downloads 50 \
-	--tmp /tmp/ \
-	--cleanup-intermediate true --nxf-unstage-hack true \
+    -params-file params.example.yaml \
 	-profile bruno,mamba
-
-
-#	--fastq-path $PWD/fastq \
-# --backup-tmp /hpc/scratch/<your_temp> --backupScratchHack true
