ba1ff78aa88c18b10183a5b2aa288a3066f8537b
ewaltari <ewaltari@gmail.com>
2026-03-27 15:06:59 -0700

    Update slurm_4_counting_anddarkmatter.sh
    removing commented-out lines

--- src/metatranscriptome/scripts/post_processing/slurm_4_counting_anddarkmatter.sh
+++ src/metatranscriptome/scripts/post_processing/slurm_4_counting_anddarkmatter.sh
@@ -19,9 +19,7 @@ module load anaconda/2023.03
 conda activate seqtk
 
 ## generic code
-
 ## focused on dark matter calcs and extracting fasta
-## /hpc/projects/balla_group/sra_experiments/RNAquarium_60k_nonhostpipeline/paired_end/chunk_001_nonzfhum.fasta
 
 
 ### key stats are taxonomy_hits_nonhost_inputcontigs_list
@@ -31,14 +29,14 @@ conda activate seqtk
 
 # Step 1: Count sequences before filter (e.g. 89.8M contigs)
 for file in chunk_[0-9][0-9][0-9].fasta; do
-    grep '^>' "$file"
+grep '^>' "$file"
 done > taxonomy_hits_prefilter_list.txt
 
 wc -l taxonomy_hits_prefilter_list.txt
 
 # Step 2: Count sequences after host filtering (e.g. 48.0M contigs)
 for file in chunk_*_nonzfhum.fasta; do
-    grep '^>' "$file"
+grep '^>' "$file"
 done > taxonomy_hits_NTNRinput_list.txt
 
 wc -l taxonomy_hits_NTNRinput_list.txt
@@ -64,8 +62,8 @@ rm -f temp_nt_query_*.txt taxonomy_hits_blastnclustered_allhits_list.txt
 # Loop through each NT BLAST result file
 cd nt_blast
 for file in chunk_*_nonzfhum.blast.txt.gz; do
-    outname="../temp_nt_query_${file%.blast.txt.gz}.txt"
-    zcat "$file" | awk -F'\t' '{print $1}' | sort | uniq > "$outname"
+outname="../temp_nt_query_${file%.blast.txt.gz}.txt"
+zcat "$file" | awk -F'\t' '{print $1}' | sort | uniq > "$outname"
 done
 cd ..
 
@@ -82,7 +80,7 @@ wc -l taxonomy_hits_blastnclustered_allhits_list.txt
 
 cd nr_diamond  # change to NR directory was chunk_*_nonzfhum.a.diamond.txt.gz
 for file in *.txt.gz; do
-    zcat "$file" | awk -F'\t' '{print $1}' >> ../temp_nr_queries.txt
+zcat "$file" | awk -F'\t' '{print $1}' >> ../temp_nr_queries.txt
 done
 cd ..
 
@@ -93,7 +91,7 @@ wc -l taxonomy_hits_diamond_allhits_list.txt
 
 ## Step 5
 cat taxonomy_hits_blastnclustered_allhits_list.txt taxonomy_hits_diamond_allhits_list.txt \
-    | sort | uniq > taxonomy_hits_NTorNR_combined_list.txt
+| sort | uniq > taxonomy_hits_NTorNR_combined_list.txt
 
 wc -l taxonomy_hits_NTorNR_combined_list.txt
 
@@ -104,7 +102,7 @@ sort taxonomy_hits_NTNRinput_list.txt > taxonomy_hits_NTNRinput_list.sorted.txt
 sed 's/^>//' taxonomy_hits_NTNRinput_list.sorted.txt > taxonomy_hits_NTNRinput_list.sortedcleaned.txt
 
 comm -23 taxonomy_hits_NTNRinput_list.sortedcleaned.txt taxonomy_hits_NTorNR_combined_list.txt \
-    > taxonomy_hits_notfoundinNTorNR_list.txt
+> taxonomy_hits_notfoundinNTorNR_list.txt
 
 wc -l taxonomy_hits_notfoundinNTorNR_list.txt
 ## need count for R - taxonomy_hits_notfoundinNTorNR_count0.txt
@@ -113,7 +111,6 @@ wc -l taxonomy_hits_notfoundinNTorNR_list.txt
 wc -l taxonomy_hits_notfoundinNTorNR_list.txt > taxonomy_hits_notfoundinNTorNR_count.txt
 sed 's/ taxonomy_hits_notfoundinNTorNR_list.txt//' taxonomy_hits_notfoundinNTorNR_count.txt > taxonomy_hits_notfoundinNTorNR_count0.txt
 
-#comm -23 ./RNAquarium_outputs/stats/taxonomy_hits_nonhost_inputcontigs_list.txt ./RNAquarium_outputs/stats/taxonomy_hits_nonhost_hitfound_list.txt | wc -l > ./RNAquarium_outputs/stats/taxonomy_hits_notfoundinNTorNR_count0.txt
 
 
 
@@ -138,17 +135,10 @@ echo "$pre $post $nt $nr $union $dark" > pipeline_counts.txt
 ## this loop will find all of the fasta files in the work directory, then run seqtk
 ## TO GET THESE DARK MATTER FASTAS, WE CAN USE NONZFHUM FASTAS (in paired_end folder)
 
-#find ./paired_end -type f -name "*nonzfhum.fasta"
-#find ./paired_end -type f -name "*hum.fasta" | while read -r fasta_file; do
-#    seqtk subseq "$fasta_file" ./RNAquarium_outputs/stats/taxonomy_hits_notfoundinNTorNR_list.txt >> #./RNAquarium_outputs/taxonomy_hits_notfoundinNTorNR_darkmatter.fasta
-#done
-
 
 for file in chunk_*_nonzfhum.fasta; do
-    seqtk subseq "$file" taxonomy_hits_notfoundinNTorNR_list.txt >> taxonomy_hits_notfoundinNTorNR_darkmatter.fasta
+seqtk subseq "$file" taxonomy_hits_notfoundinNTorNR_list.txt >> taxonomy_hits_notfoundinNTorNR_darkmatter.fasta
 done
-
-### final ideas - to get the filtered list per killifish code, then create a simple workflow figure incorporating all of these numbers using pikchr! would also need count of contigs input into first blast...
 
 ## can add plotting steps via R commands invoked last
 
@@ -156,8 +146,7 @@ done
 sleep 5
 module purge
 module load R/4.4
-#R
-#source("nr_diamond_processing.r")
+
 Rscript 4_alluvial_withdarkmatter.r
 
 sleep 5
@@ -166,17 +155,17 @@ mkdir -p stats
 cd ..
 
 mv taxonomy_hits_prefilter_list.txt RNAquarium_outputs/stats/
-mv taxonomy_hits_NTNRinput_list.txt RNAquarium_outputs/stats/
-mv taxonomy_hits_blastnclustered_allhits_list.txt RNAquarium_outputs/stats/
-mv taxonomy_hits_diamond_allhits_list.txt RNAquarium_outputs/stats/
-mv taxonomy_hits_NTorNR_combined_list.txt RNAquarium_outputs/stats/
-mv taxonomy_hits_notfoundinNTorNR_count.txt RNAquarium_outputs/stats/
-mv taxonomy_hits_notfoundinNTorNR_count0.txt RNAquarium_outputs/stats/
-
-mv taxonomy_hits_NTNRinput_list.sorted.txt RNAquarium_outputs/stats/
-mv taxonomy_hits_NTNRinput_list.sortedcleaned.txt RNAquarium_outputs/stats/
-mv taxonomy_hits_notfoundinNTorNR_list.txt RNAquarium_outputs/stats/
-mv pipeline_counts.txt RNAquarium_outputs/stats/
-
-#mv taxonomy_hits_notfoundinNTorNR_list.txt stats/
-mv taxonomy_hits_notfoundinNTorNR_darkmatter.fasta RNAquarium_outputs/
+  mv taxonomy_hits_NTNRinput_list.txt RNAquarium_outputs/stats/
+  mv taxonomy_hits_blastnclustered_allhits_list.txt RNAquarium_outputs/stats/
+  mv taxonomy_hits_diamond_allhits_list.txt RNAquarium_outputs/stats/
+  mv taxonomy_hits_NTorNR_combined_list.txt RNAquarium_outputs/stats/
+  mv taxonomy_hits_notfoundinNTorNR_count.txt RNAquarium_outputs/stats/
+  mv taxonomy_hits_notfoundinNTorNR_count0.txt RNAquarium_outputs/stats/
+  
+  mv taxonomy_hits_NTNRinput_list.sorted.txt RNAquarium_outputs/stats/
+  mv taxonomy_hits_NTNRinput_list.sortedcleaned.txt RNAquarium_outputs/stats/
+  mv taxonomy_hits_notfoundinNTorNR_list.txt RNAquarium_outputs/stats/
+  mv pipeline_counts.txt RNAquarium_outputs/stats/
+  
+  #mv taxonomy_hits_notfoundinNTorNR_list.txt stats/
+  mv taxonomy_hits_notfoundinNTorNR_darkmatter.fasta RNAquarium_outputs/
