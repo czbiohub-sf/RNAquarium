5b8b5725e64b3471913eb3173b8d2a140d1ee104
ewaltari <ewaltari@gmail.com>
2026-03-27 15:00:58 -0700

    Update 4_alluvial_withdarkmatter.r
    removing commented-out lines

--- src/metatranscriptome/scripts/post_processing/4_alluvial_withdarkmatter.r
+++ src/metatranscriptome/scripts/post_processing/4_alluvial_withdarkmatter.r
@@ -43,14 +43,11 @@ statspath <- str_c(outpath, "/stats")
 
 ### read in all files first, then set output path to virus subfolder...
 alluvialfiles <- fs::dir_ls(glob="taxonomy_hits_nonhost_alluvialplot_counts*", recurse = FALSE)
-## chunk names
-#alluvialfile <- alluvialfiles[1]
 ## instead use more recent file
 alluvialfile <- alluvialfiles[which.max(file_info(alluvialfiles)$modification_time)]
 
 
 data_alluvialb0 <- read_tsv(alluvialfile)
-#write.table(data_alluviala, file = paste0("taxonomy_hits_nonhost_alluvialplot_counts_",Sys.Date(),".tsv"), sep = "\t", row.names = FALSE, quote = FALSE)
 
 
 #####
@@ -58,24 +55,11 @@ data_alluvialb0 <- read_tsv(alluvialfile)
 #### retain old order with missing last
 data_alluviala <- data_alluvialb0 %>%
   mutate(across(everything(), ~replace_na(.x, "Missing")))
-#data_alluviala <- data_alluviala %>% rename(NT = taxoncategorysimple_NTclustered)
-#data_alluviala <- data_alluviala %>% rename(NR = taxoncategorysimple_NR)
-#data_alluviala <- data_alluviala %>% rename(used_NTorNR = taxoncategorysimple_NTorNR)
 
 sumcountonepercent <- (sum(data_alluviala$count) / 100)
 ## set of order of each category using a factor??
 data_alluviala <- data_alluviala %>% arrange(desc(count))
 
-### alternate plan for using treemap order, out to 11
-#data_heatmap0 <- data_alluviala %>%
-#  group_by(used_NTorNR) %>%
-#  summarise(totalcount = sum(count)) %>%
-#  ungroup()
-#data_heatmap0 <- data_heatmap0 %>% rename(taxoncategorysimple_NTorNR = used_NTorNR)
-#data_heatmap0 <- data_heatmap0 %>% rename(count = totalcount)
-#alluvialorder11o <- data_heatmap0 %>% arrange(desc(count)) %>% slice_head(n = 11)
-#alluvialorder11 <- unique(alluvialorder11o$taxoncategorysimple_NTorNR)
-#alluvialorder11[12] <- "Missing"
 
 ## better code because sometimes totalcount order isn't order of categories in used_NTorNR
 used_category_totals <- data_alluviala %>%
@@ -83,7 +67,6 @@ used_category_totals <- data_alluviala %>%
   summarise(total_count = sum(count), .groups = 'drop') %>%
   arrange(desc(total_count)) %>%
   slice_head(n = 11)
-#top_used_categories <- used_category_totals$used_NTorNR
 alluvialorder11 <- used_category_totals$used_NTorNR
 alluvialorder11[12] <- "Missing"
 
@@ -92,29 +75,12 @@ used_category_totals <- data_alluviala %>%
   group_by(used_NTorNR) %>%
   summarise(total_count = sum(count), .groups = 'drop') %>%
   arrange(desc(total_count))
-#top_used_categories <- used_category_totals$used_NTorNR
 alluvialorder14 <- used_category_totals$used_NTorNR
 alluvialorder14[15] <- "Missing"
 
 ##############################
 ## then the new dark matter count to add at end - only saving diagram there but also in main script move files to statspath as well
 setwd(statspath)
-
-## ADDING IN A SINGLE VALUE FROM A COUNT.TXT FILE...
-
-# Read the integer from count.txt do this at start in main folder!
-#count_value <- as.numeric(readLines("taxonomy_hits_notfoundinNTorNR_count0.txt"))
-
-# Create a data frame with one row and the specified column names
-
-# data_alluvialb1 <- data.frame(
-#   taxoncategorysimple_NTclustered = "Missing",
-#   taxoncategorysimple_NTorNR = "Missing",
-#   taxoncategorysimple_NR = "Missing",
-#   count = count_value,
-#   stringsAsFactors = FALSE
-# )
-
 
 
 data_alluvialb1 <- data.frame(
@@ -130,22 +96,9 @@ print(data_alluvialb1)
 
 
 ## now alluvial plots & treemaps
-
 setwd(outpath)
 
 data_alluvialadm <- bind_rows(data_alluvialb0,data_alluvialb1)
-#rm(data_alluvialb0)
-#rm(data_alluvialb1)
-
-
-
-#data_alluvialadm <- data_alluvialadm %>% rename(NT = taxoncategorysimple_NTclustered)
-#data_alluvialadm <- data_alluvialadm %>% rename(NR = taxoncategorysimple_NR)
-#data_alluvialadm <- data_alluvialadm %>% rename(used_NTorNR = taxoncategorysimple_NTorNR)
-
-#sumcountonepercent <- (sum(data_alluviala$count) / 100)
-#sumcountonepercentdm <- (sum(data_alluvialadm$count) / 100)
-#sumcountonepercent <- (sum(data_alluvialadm$count) / 100)
 
 ## set of order of each category using a factor??
 data_alluvialadm <- data_alluvialadm %>% arrange(desc(count))
@@ -157,12 +110,7 @@ data_alluvialdm <- data_alluvialadm %>% dplyr::filter(count > sumcountonepercent
 
 
 ## now to figure out 11 hues
-#library(colorspace)
 cl = c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999", "#FF62BC", "#A3A500")
-#lightcl <- lighten(cl, amount = 0.3)
-#darkcl <- darken(cl, amount = 0.3)
-#[1] "#9F1718" "#2D577C" "#397737" "#693870" "#B05601" "#ACAC00" "#743C1E" "#D32092" "#696969" "#CC018C" "#6F7105"
-# scale_fill_manual(values = c( "#00969A", "#4D6E00", "#C12600", "#9E00DF")) + ## 0.3
 darkcl <- c("#9F1718", "#2D577C", "#397737", "#693870", "#B05601", "#ACAC00", "#743C1E", "#D32092", "#696969", "#CC018C", "#6F7105")
 lightcl <- c("#FF6B6B", "#69A4DF", "#6ECD6C", "#C17CCD", "#FFA984", "#FFFF91", "#D4805B", "#FFA7D2", "#B7B7B7", "#FF9ACE", "#BFC144")
 
@@ -172,18 +120,10 @@ cl12 = c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65
 darkcl12 <- c("#9F1718", "#2D577C", "#397737", "#693870", "#B05601", "#ACAC00", "#743C1E", "#D32092", "#696969", "#36454F", "#36454F", "#36454F")
 lightcl12 <- c("#FF6B6B", "#69A4DF", "#6ECD6C", "#C17CCD", "#FFA984", "#FFFF91", "#D4805B", "#FFA7D2", "#B7B7B7", "#36454F", "#36454F", "#36454F")
 
-## expanded palette with 14 hues - but dark gray last
-#lightcl14 <- c("#FF6B6B", "#69A4DF", "#6ECD6C", "#C17CCD", "#FFA984", "#FFFF91", 
-#             "#D4805B", "#FFA7D2", "#B7B7B7", "#FF9ACE", "#BFC144",
-#             "#B3E5FC", "#FFCC80", "#36454F")  # 3 new colors
 cl14 <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", 
-             "#A65628", "#F781BF", "#999999", "#FF62BC", "#A3A500",
-             "#36454F", "#36454F", "#36454F")  # more gray
+          "#A65628", "#F781BF", "#999999", "#FF62BC", "#A3A500",
+          "#36454F", "#36454F", "#36454F")  # more gray
 
-#data_alluvialdm$NT <- factor(data_alluvialdm$NT, levels = alluvialorder11, ordered = TRUE)
-#data_alluvialdm$NR <- factor(data_alluvialdm$NR, levels = alluvialorder11, ordered = TRUE)
-#data_alluvialdm$used_NTorNR <- factor(data_alluvialdm$used_NTorNR, levels = alluvialorder11, ordered = TRUE)
-## this works, but also have to remove "decreasing = FALSE" across the commands
 ## update to 14 levels
 data_alluvialdm$NT <- factor(data_alluvialdm$NT, levels = alluvialorder14, ordered = TRUE)
 data_alluvialdm$NR <- factor(data_alluvialdm$NR, levels = alluvialorder14, ordered = TRUE)
@@ -207,17 +147,14 @@ alluvialdm_plotall <- ggplot(data_alluvialdm, aes(axis1 = NT, axis2 = used_NTorN
        x = "", y = "Count") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 16))
 
-#alluvialdm_plotall
-
 
 ggsave(filename = paste("taxonomy_hits_nonhost_alluvialplot_all_withdarkmatter_",Sys.Date(),".png", sep=""), alluvialdm_plotall, width = 18, height = 9, units = "in", limitsize = FALSE)
-#ggsave(filename = paste("allchunks_alluvialplot_all0_",Sys.Date(),".png", sep=""), alluvial_plotall, width = 5.6, height = 3.4, units = "in", limitsize = FALSE)
 ggsave(filename = paste("taxonomy_hits_nonhost_alluvialplot_all_withdarkmatter_",Sys.Date(),".pdf", sep=""), alluvialdm_plotall, width = 18, height = 9, units = "in", limitsize = FALSE)
 
 ################################################
 ######## final bar plot ##########
 used_category_totals <- data_alluviala %>% group_by(used_NTorNR) %>% summarise(n = sum(count)) %>% arrange(desc(n))
-## remove mollusca annelida platyhelminthes dplyr::filter(viruscategorysimple_NTorNR != "Phage")
+## remove mollusca annelida platyhelminthes 
 used_category_totals11 <- used_category_totals %>% dplyr::filter(used_NTorNR != "Mollusca")
 used_category_totals11 <- used_category_totals11 %>% dplyr::filter(used_NTorNR != "Platyhelminthes")
 used_category_totals11 <- used_category_totals11 %>% dplyr::filter(used_NTorNR != "Annelida")
@@ -229,16 +166,6 @@ lightcl11 <- c("#FF6B6B", "#69A4DF", "#6ECD6C", "#C17CCD", "#FFA984", "#FFFF91",
                "#D4805B", "#FFA7D2", "#FF9ACE", "#BFC144",
                "#B3E5FC")
 
-# ggplot(used_category_totals11, aes(x = used_NTorNR, y = n)) +
-#   geom_col() + scale_fill_manual(values = lightcl11) +
-#   scale_y_log10(labels = scales::comma) +
-#   labs(
-#     x = "Category",
-#     y = "Count (log scale)",
-#     title = "Distribution across taxonomic categories"
-#   ) +
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 contig_barplot <- ggplot(used_category_totals11, aes(x = used_NTorNR, y = n, fill = used_NTorNR)) +
   geom_col() + 
@@ -257,19 +184,10 @@ ggsave(filename = paste("taxonomy_hits_nonhost_taxoncategories_barplot_",Sys.Dat
 ggsave(filename = paste("taxonomy_hits_nonhost_taxoncategories_barplot_",Sys.Date(),".pdf", sep=""), contig_barplot, width = 9, height = 9, units = "in", limitsize = FALSE)
 
 ############################################################
-
-### ### ### ### ### ### ### ### ### ### ### ### ### 
 ### adding little pipeline plot
 
 setwd(statspath)
 
-
-# Read values from pipeline_counts.txt
-#counts <- scan("pipeline_counts.txt")
-#names(counts) <- c(
-#  "pre_filter", "post_filter", "nt_hits", 
-#  "nr_hits", "nt_or_nr", "dark_matter"
-#)
 
 # Convert to millions
 counts_m <- round(counts / 1e6, 1)
@@ -278,32 +196,32 @@ counts_m <- round(counts / 1e6, 1)
 draw_flowchart <- function() {
   par(mar = c(1, 1, 1, 1))
   plot(1, type = "n", xlim = c(0, 15), ylim = c(-2, 9), axes = FALSE, xlab = "", ylab = "")
-
+  
   draw_box <- function(x, y, label, count) {
     rect(x, y, x + 2, y + 1.2, col = "lightgray")
     text(x + 1, y + 0.6, label, cex = 1.1, font = 2)
     text(x + 1, y - 0.2, paste0(count, "M contigs"), cex = 1)
   }
-
+  
   draw_arrow <- function(x1, y1, x2, y2) {
     arrows(x1, y1, x2, y2, length = 0.12, lwd = 1.5)
   }
-
+  
   # Boxes and arrows
   draw_box(1, 5, "Assemble\ninto contigs", counts_m["pre_filter"])
   draw_arrow(3, 5.6, 4.5, 5.6)
   draw_box(4.5, 5, "Filter Danio\n& human", counts_m["post_filter"])
-
+  
   draw_arrow(6.5, 5.6, 8.5, 7)
   draw_box(8.5, 7, "NT blast", counts_m["nt_hits"])
-
+  
   draw_arrow(6.5, 5.6, 8.5, 3)
   draw_box(8.5, 3, "NR diamond", counts_m["nr_hits"])
-
+  
   draw_arrow(10.5, 7.6, 12.5, 5.6)
   draw_arrow(10.5, 3.6, 12.5, 5.6)
   draw_box(12.5, 5, "In NT or NR", counts_m["nt_or_nr"])
-
+  
   draw_box(12.5, 1, "NOT in NT or NR\n(dark matter)", counts_m["dark_matter"])
 }
 
